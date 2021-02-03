fd887d6936b2b834ac276ca7fc884ea311e1e3e1
Pratik Solim <pratik.solim@gmail.com>
2021-02-04 01:57:37 +0530

    Added new Node insertion method

--- data-structures/tree/bst.js
+++ data-structures/tree/bst.js
@@ -8,7 +8,32 @@ class Node {
 
 class BinarySearchTree {
 	constructor(rootValue) {
-			this.root = new Node(rootValue);
+		this.root = new Node(rootValue)
+	}
+
+	insert(val) {
+		if (this.root === null) {
+			this.root = new Node(val)
+			return
+		}
+
+		const currNode = this.root
+		let parent
+		while (currNode) {
+			parent = currNode
+
+			if (val < currNode.val) {
+				currNode = currNode.leftChild
+			} else {
+				currNode = currNode.rightChild
+			}
+		}
+
+		if (val < parent.val) {
+			parent.leftChild = new Node(val)
+		} else {
+			parent.rightChild = new Node(val)
+		}
 	}
 }
 
